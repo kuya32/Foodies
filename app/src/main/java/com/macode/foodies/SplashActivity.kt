fcb6a25ed85cf748d633b9e32aff109c20f2cb76
Marchael Acode <m.acode@outlook.com>
2021-04-15 09:48:55 -0700

    Created a splash screen activity where the title is shown through an animation.

---
+++ app/src/main/java/com/macode/foodies/SplashActivity.kt
@@ -0,0 +1,39 @@
+package com.macode.foodies
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import com.macode.foodies.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val splashAnimation = AnimationUtils.loadAnimation(this, R.anim.anim_splash)
+        binding.splashScreenTitle.animation = splashAnimation
+        binding.splashScreenImage.animation = splashAnimation
+        splashAnimation.setAnimationListener(object: Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                Handler(Looper.getMainLooper()).postDelayed({
+                    startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                    finish()
+                }, 1000)
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+
+            }
+
+        })
+    }
+}
