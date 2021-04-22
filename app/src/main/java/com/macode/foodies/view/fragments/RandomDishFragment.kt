615fe253da61430eb51691f1431a56c49ce00ea3
Marchael Acode <m.acode@outlook.com>
2021-04-22 12:45:52 -0700

    Created the random dish UI and connected the binding functionality to fragment.

--- app/src/main/java/com/macode/foodies/view/fragments/RandomDishFragment.kt
+++ app/src/main/java/com/macode/foodies/view/fragments/RandomDishFragment.kt
@@ -1,18 +1,22 @@
 package com.macode.foodies.view.fragments
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.macode.foodies.R
 import com.macode.foodies.databinding.FragmentRandomDishBinding
 import com.macode.foodies.viewmodel.NotificationsViewModel
 
 class RandomDishFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
+    private var binding: FragmentRandomDishBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,12 +26,23 @@ class RandomDishFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        notificationsViewModel = ViewModelProvider(this).get(NotificationsViewModel::class.java)
-        val binding = FragmentRandomDishBinding.inflate(inflater, container, false)
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
-            binding.notificationsText.text = it
-        })
+        binding = FragmentRandomDishBinding.inflate(inflater, container, false)
+        val view = binding!!.root
 
-        return binding.root
+        setUpToolbar(view)
+
+        return view
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+
+    private fun setUpToolbar(view: View) {
+        val toolbar =  view.findViewById<Toolbar>(R.id.randomDishToolbar)
+        (requireActivity() as AppCompatActivity).setSupportActionBar(toolbar)
+        (requireActivity() as AppCompatActivity).supportActionBar?.title = "Random Dish"
+        toolbar.setTitleTextColor(Color.parseColor("#FFFFFF"))
     }
 }
