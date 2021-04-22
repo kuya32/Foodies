d6189ce4d309069628353c432e9bbe29f1f4b64f
Marchael Acode <m.acode@outlook.com>
2021-04-22 15:09:46 -0700

    Populating the UI with the data from the API and implementing swipe to refresh functionality.

--- app/src/main/java/com/macode/foodies/view/fragments/RandomDishFragment.kt
+++ app/src/main/java/com/macode/foodies/view/fragments/RandomDishFragment.kt
@@ -1,22 +1,34 @@
 package com.macode.foodies.view.fragments
 
 import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.text.Html
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.lifecycle.Observer
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
 import com.macode.foodies.R
+import com.macode.foodies.application.FavDishApplication
 import com.macode.foodies.databinding.FragmentRandomDishBinding
-import com.macode.foodies.viewmodel.NotificationsViewModel
+import com.macode.foodies.model.entities.FavDish
+import com.macode.foodies.model.entities.RandomDish
+import com.macode.foodies.utilities.Constants
+import com.macode.foodies.viewmodel.FavDishViewModel
+import com.macode.foodies.viewmodel.FavDishViewModelFactory
+import com.macode.foodies.viewmodel.RandomDishViewModel
 
 class RandomDishFragment : Fragment() {
 
     private var binding: FragmentRandomDishBinding? = null
+    private lateinit var randomDishViewModel: RandomDishViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,9 +46,104 @@ class RandomDishFragment : Fragment() {
         return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        randomDishViewModel = ViewModelProvider(this@RandomDishFragment).get(RandomDishViewModel::class.java)
+
+        randomDishViewModel.getRandomRecipeFromAPI()
+
+        randomDishViewModelObserver()
+
+        binding!!.swipeRefreshRandomDishLayout.setOnRefreshListener {
+            randomDishViewModel.getRandomRecipeFromAPI()
+        }
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         binding = null
+    }
+
+    private fun randomDishViewModelObserver() {
+        randomDishViewModel.randomDishResponse.observe(viewLifecycleOwner, {randomDishResponse ->
+            randomDishResponse?.let {
+            setRandomDishResponseInUI(randomDishResponse.recipes[0])
+        }})
+        randomDishViewModel.randomDishLoadingError.observe(viewLifecycleOwner, {dataError ->
+            dataError?.let {
+
+        }})
+
+        randomDishViewModel.loadRandomDish.observe(viewLifecycleOwner, {loadRandomDish ->
+            loadRandomDish?.let {
+
+        }})
+    }
+
+    private fun setRandomDishResponseInUI(recipe: RandomDish.Recipe) {
+        Glide
+            .with(requireActivity())
+            .load(recipe.image)
+            .centerCrop()
+            .into(binding!!.randomDishImage)
+
+        binding!!.randomDishTitle.text = recipe.title
+
+        var dishType: String = "Other"
+        if (recipe.dishTypes.isNotEmpty()) {
+            dishType = recipe.dishTypes[0]
+            binding!!.randomDishType.text = dishType
+        }
+
+        binding!!.randomDishCategory.text = "Other"
+
+        var ingredients = ""
+        for (value in recipe.extendedIngredients) {
+            if (ingredients.isEmpty()) {
+                ingredients = value.original
+            } else {
+                ingredients = ingredients + ", \n" + value.original
+            }
+        }
+        binding!!.randomDishIngredient.text = ingredients
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            binding!!.randomDishDirection.text = Html.fromHtml(
+                recipe.instructions,
+                Html.FROM_HTML_MODE_COMPACT
+            )
+        } else {
+            @Suppress("DEPRECATION")
+            binding!!.randomDishDirection.text = Html.fromHtml(recipe.instructions)
+        }
+
+        binding!!.randomDishCookingTime.text = resources.getString(R.string.estimateCookingTime, recipe.readyInMinutes.toString())
+
+        binding!!.favoriteButton.setOnClickListener {
+            val randomDishDetails = FavDish(
+                recipe.image,
+                Constants.DISH_IMAGE_SOURCE_ONLINE,
+                recipe.title,
+                dishType,
+                "Other",
+                ingredients,
+                recipe.readyInMinutes.toString(),
+                recipe.instructions,
+                true
+            )
+
+            val favDishViewModel: FavDishViewModel by viewModels {
+                FavDishViewModelFactory((requireActivity().application as FavDishApplication).repository)
+            }
+            favDishViewModel.insert(randomDishDetails)
+
+            binding!!.favoriteButton.setImageDrawable(
+                ContextCompat.getDrawable(requireActivity(), R.drawable.favorite_selected)
+            )
+            Toast.makeText(requireActivity(), "Dish added to favorites!", Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 
     private fun setUpToolbar(view: View) {
